9f934074c6c449b6516766346c94728fd88a19c1
Neil Crawford <156959947+dundura@users.noreply.github.com>
2026-04-12 16:09:18 -0400

    Add AFC 2016 Silver getting started page and team link

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -21,6 +21,7 @@ const TEAM_PAGES = [
   { title: '2015 Legacy Girls', slug: '2015-legacy-girls-getting-started' },
   { title: 'Ambassadors Football Club 2012 Boys', slug: 'amfc-2012-boys-getting-started' },
   { title: 'Ambassadors FC', slug: 'ambassadors-fc-girls-getting-started', aliases: ['Ambassadors 2012 Girls Blue', 'Ambassadors 2012 White'] },
+  { title: 'AFC 2016 Silver', slug: 'afc2016silver', aliases: ['afc2016silver'] },
   { title: 'AFC', slug: 'getting-started-afc' },
   { title: 'Brian Chongtoua | Private Soccer Specialist', slug: 'bc-getting-started' },
   { title: 'Avalanche', slug: 'avalanche-getting-started' },
