c08406431549eaf946575ca9143e4bbdcf7d7e36
Neil Crawford <156959947+dundura@users.noreply.github.com>
2026-05-21 23:24:17 -0400

    Nudge CTA button right

--- src/components/SaleBannerStrip.tsx
+++ src/components/SaleBannerStrip.tsx
@@ -48,7 +48,7 @@ export default function SaleBannerStrip() {
                 </p>
               </div>
               {/* CTA */}
-              <div className="flex-shrink-0 text-center">
+              <div className="flex-shrink-0 text-center md:ml-8">
                 <a
                   href={SIGNUP_URL}
                   target="_blank"
