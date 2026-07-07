0f353b9d851d70730bcb55a7ba1f57040fc45fd8
Neil Crawford <156959947+dundura@users.noreply.github.com>
2026-07-06 23:02:12 -0400

    Add WJFC BU13 AM getting started page
    Cloned from the WJFC JG page with team name/code swapped for
    WJFC BU13 AM (wjfcam07062026). Also listed on /teams.

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -59,6 +59,7 @@ const TEAM_PAGES = [
   { title: 'PFC Tsunami 14G', slug: 'pfc-tsunami-14g-getting-started' },
   { title: 'Pacific FC 14G Lightning', slug: 'pacific-fc-14g-lightning-getting-started' },
   { title: 'Pacific FC 17G Spirit', slug: 'pacific-fc-17g-spirit-getting-started' },
+  { title: 'WJFC BU13 AM', slug: 'wjfc-bu13-am-getting-started' },
 ];
 
 const PARENT_GUIDES = [
